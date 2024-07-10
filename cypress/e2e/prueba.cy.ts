a181a915b034de801e9d2c0f92af84c08e3b344c
Victor Estrada <victor.estrada@himedsolutions.com>
2024-07-10 08:56:57 -0500

    test

--- cypress/e2e/prueba.cy.ts
+++ cypress/e2e/prueba.cy.ts
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    cy.location("pathname").should('eq', '/web/index.php/auth/login');
   })
 })
